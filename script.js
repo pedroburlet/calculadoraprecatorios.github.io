6f41c7cf3580b2f608da4df48c7e99cac46e2fcc
pedroburlet <113491623+pedroburlet@users.noreply.github.com>
2024-06-24 16:55:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ function converterParaNumero(valor) {
     return parseFloat(valor.replace(/\./g, '').replace(',', '.'));
 }
 
+// Função para formatar a entrada de texto como reais (R$) durante a digitação
+function formatarEntrada(campo) {
+    let valor = campo.value;
+    valor = valor.replace(/\D/g, '');
+    valor = (valor / 100).toFixed(2) + '';
+    valor = valor.replace(".", ",");
+    valor = valor.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+    campo.value = valor;
+}
+
 // Função para calcular a proposta
 function calcularProposta() {
     // Obtendo os valores do formulário e convertendo para números
@@ -16,23 +26,30 @@ function calcularProposta() {
     const valorAtualizadoTJRJ = converterParaNumero(document.getElementById('valorAtualizadoTJRJ').value);
     const percentHonorarios = parseFloat(document.getElementById('percentHonorarios').value.replace(',', '.'));
     const impostoRenda = document.getElementById('impostoRenda').value;
+    const descontoPrioridade = document.getElementById('descontoPrioridade').value;
     const ano = parseInt(document.getElementById('ano').value);
 
-    // Passo 1: Calculando e arredondando a porcentagem da previdência
+    // Passo 1: Aplicando o desconto de prioridade se necessário
+    let valorComDescontoPrioridade = valorAtualizadoTJRJ;
+    if (descontoPrioridade === 'sim') {
+        valorComDescontoPrioridade -= 141000;
+    }
+
+    // Passo 2: Calculando e arredondando a porcentagem da previdência
     let porcentagemPrevidencia = (valorPrevidenciaOficio / valorBrutoOficio) * 100;
     porcentagemPrevidencia = Math.round(porcentagemPrevidencia);
     document.getElementById('resultadoPrevidenciaPorcentagem').innerText = `${porcentagemPrevidencia}%`;
 
-    // Passo 2: Tirando a % dos honorários do valor atualizado
-    const valorMenosHonorarios = valorAtualizadoTJRJ * (1 - percentHonorarios / 100);
+    // Passo 3: Tirando a % dos honorários do valor atualizado com desconto de prioridade
+    const valorMenosHonorarios = valorComDescontoPrioridade * (1 - percentHonorarios / 100);
     document.getElementById('resultadoHonorarios').innerText = formatarReais(valorMenosHonorarios);
 
-    // Passo 3: Tirando a porcentagem da previdência do valor atualizado
+    // Passo 4: Tirando a porcentagem da previdência do valor atualizado
     const descontoPrevidencia = valorMenosHonorarios * (porcentagemPrevidencia / 100);
     const valorMenosPrevidencia = valorMenosHonorarios - descontoPrevidencia;
     document.getElementById('resultadoPrevidencia').innerText = `Valor líquido: ${formatarReais(valorMenosPrevidencia)} \n Valor descontado: ${formatarReais(descontoPrevidencia)}`;
 
-    // Passo 4: Aplicando o imposto de renda se necessário
+    // Passo 5: Aplicando o imposto de renda se necessário
     let valorAposImpostoRenda = valorMenosPrevidencia;
     let valorDescontoIR = 0;
     if (impostoRenda === 'sim') {
@@ -40,10 +57,10 @@ function calcularProposta() {
         valorAposImpostoRenda = valorMenosPrevidencia - valorDescontoIR;
         document.getElementById('resultadoImpostoRenda').innerText = `Valor líquido atualizado: ${formatarReais(valorAposImpostoRenda)} \n Valor descontado: ${formatarReais(valorDescontoIR)}`;
     } else {
-        document.getElementById('resultadoImpostoRenda').innerText = `Valor líquido atualizado: ${formatarReais(valorAposImpostoRenda)} \n Valor descontado: R$ 0,00`;
+        document.getElementById('resultadoImpostoRenda').innerText = `Valor líquido atualizado: ${formatarReais(valorAposImpostoRenda)} \n Valor descontado: ${formatarReais(0)}`;
     }
 
-    // Passo 5: Calculando a proposta para o ano
+    // Passo 6: Calculando a proposta para o ano
     let valorProposta;
     switch (ano) {
         case 2021:
@@ -68,11 +85,7 @@ function calcularProposta() {
 
     if (!isNaN(valorProposta)) {
         document.getElementById('resultadoAno').innerText = formatarReais(valorProposta);
-        document.getElementById('resultadoFinal').innerText = formatarReais(valorProposta);
     } else {
         document.getElementById('resultadoAno').innerText = `Ano inválido`;
-        document.getElementById('resultadoFinal').innerText = `Ano inválido`;
     }
-
 }
-
